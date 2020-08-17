4310bf319658ed5ea5cfcd98ded475089091fcba
Massimo <massimo.rosas279@gmail.com>
2020-08-17 19:06:09 +0200

    Try Spotify login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,73 @@
 // == Import npm
-import React from 'react';
+import React, { Component } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 // == Import
+import './index.scss';
+
+const spotifyApi = new SpotifyWebApi();
 
 // == Composant
-const App = () => (
-  <div className="app">
-    Spot-Info
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    console.log(params);
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
+    };
+  }
+
+  getHashParams = () => {
+    const hashParams = {};
+    let e = /([^&;=]+)=?([^&;]*)/g;
+    const q = window.location.hash.substring(1);
+    const r = /([^&;=]+)=?([^&;]*)/g;
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url,
+          },
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <a href="http://localhost:8888">
+          <button className="login-button" type="button">Login to Spotify</button>
+        </a>
+        <div>
+          Now Playing: {this.state.nowPlaying.name}
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} alt="album-cover" style={{ height: 150 }} />
+        </div>
+        { this.state.loggedIn && (
+          <button type="button" onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 // == Export
 export default App;
